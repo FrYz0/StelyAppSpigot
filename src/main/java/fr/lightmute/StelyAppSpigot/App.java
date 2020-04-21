cd1963f13226d474a5ebc1ea28ab1ba692fd21c3
LightMute <dylanbusque10@gmail.com>
2020-04-21 21:39:18 +0200

    Add logger démarage/arret du plugin

--- src/main/java/fr/lightmute/StelyAppSpigot/App.java
+++ src/main/java/fr/lightmute/StelyAppSpigot/App.java
@@ -1,5 +1,7 @@
 package fr.lightmute.StelyAppSpigot;
 
+import java.util.logging.Level;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class App extends JavaPlugin {
@@ -7,8 +9,9 @@ public class App extends JavaPlugin {
 	//totog
 	public void onEnable() {
 		//test
+		getLogger().log(Level.INFO, "StelyAppSpigot activé !");
 	}
 	public void onDisable() {
-
+		getLogger().log(Level.INFO, "StelyAppSpigot désactivé !");
 	}
 }
